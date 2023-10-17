d590b0f35477f0b3c40cde222599cbd54390dea4
Oleg <schika145@gmail.com>
2023-10-17 15:57:18 +0500

    Конец

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Принимает на вход трёхзначное число, на выходе показывает вторую цифру
+Console.WriteLine("Введите трёхзначное число");
+string namb = Console.ReadLine();
+int namber = Convert.ToInt32(namb);
+
+int a = namber / 10 % 10;
+Console.WriteLine(a);
